fe0459069af10d1366c4f4df4d0e392434baa1de
naga <thanhdatbkdn14@gmail.com>
2021-01-05 03:52:09 +0000

    modified:   models/library_book.py 	modified:   views/library_book.xml

--- models/library_book.py
+++ models/library_book.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
 	_name = 'library.book'
-	_description = 'Library Book'
+        _description = 'Library Book'
 	
 	name = fields.Char('Title', required=True)
 	date_release = fields.Date('Release Date')
