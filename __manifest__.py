41359d858c3cb29b70c36aee2de4c6530863f165
Naga <thanhdatbkdn14@gmail.com>
2021-01-05 09:22:52 +0700

    modified:   __manifest__.py 	modified:   models/library_book.py 	renamed:    views/views.xml -> report/report.xml 	new file:   security/ir.model.access.csv 	new file:   views/library_book.xml

--- __manifest__.py
+++ __manifest__.py
@@ -13,9 +13,9 @@ Description related to library.
 	'version': '14.0.1',
 	'depends': ['base'],
 	# This data files will be loaded at the installation (commented because file is not added in this example)
-	# 'data': [
-	#	'view.xml'
-	#],
+	 'data': [
+		'views/library_book.xml'
+	],
 	# This demo data files wll be loaded if db initialize with demo data (commented because file is not added in this example)
 	#'demo': [
 	#	'demo.xml'
