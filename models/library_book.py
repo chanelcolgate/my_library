5000576b8d9e3d294e6bcbb4e938fc55ee60263d
naga <thanhdatbkdn14@gmail.com>
2021-01-05 09:25:26 +0000

    modified:   models/library_book.py

--- models/library_book.py
+++ models/library_book.py
@@ -1,15 +1,15 @@
 from odoo import models, fields
 
 class LibraryBook(models.Model):
-	_name = 'library.book'
+    _name = 'library.book'
     _description = 'Library Book'
     _rec_name = 'short_name'
     _order = 'date_release desc,name'
 	
-	name = fields.Char('Title', required=True)
-	short_name = fields.Char('Short Title', required=True)
-	date_release = fields.Date('Release Date')
-	author_ids = fields.Many2many(
-		'res.partner',
-		string='Authors'
-	)
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+    'res.partner',
+    string='Authors'
+    )
