9aaf0c5c12b0741b2c3eebfc139c33d66cb3bf75
Naga <thanhdatbkdn14@gmail.com>
2021-01-18 10:47:58 +0700

    modified:   controllers/main.py

--- controllers/main.py
+++ controllers/main.py
@@ -36,13 +36,13 @@ class Main(http.Controller):
 		return html_result
 
 	@http.route('/my_library/book_details', type='http', auth='none')
-    def book_details(self, book_id):
-        record = request.env['library.book'].sudo().browse(int(book_id))
-        return u'<html><body><h1>%s</h1>Authors: %s' % (
-            record.name,
-            u', '.join(record.author_ids.mapped('name')) or 'none',
-        )
+	def book_details(self, book_id):
+		record = request.env['library.book'].sudo().browse(int(book_id))
+		return u'<html><body><h1>%s</h1>Authors: %s' % (
+			record.name,
+			u', '.join(record.author_ids.mapped('name')) or 'none',
+		)
 
-    @http.route("/my_library/book_details/<model('library.book'):book>", type='http', auth='none')
-    def book_details_in_path(self, book):
-        return self.book_details(book.id)
+	@http.route("/my_library/book_details/<model('library.book'):book>", type='http', auth='none')
+	def book_details_in_path(self, book):
+		return self.book_details(book.id)
