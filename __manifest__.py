885f348896107091d5960bcca51d9316f20ba8d4
Naga <thanhdatbkdn14@gmail.com>
2021-01-19 09:04:16 +0700

    modified:   __manifest__.py 	new file:   static/src/css/my_library.css 	new file:   static/src/js/my_library.js 	new file:   static/src/scss/my_library.scss 	new file:   views/templates.xml

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@ Description related to library.
 	'website': "http://www.example.com",
 	'category': 'Tools',
 	'version': '14.0.1',
-	'depends': ['base', 'contacts'],
+	'depends': ['base', 'contacts', 'website'],
 	# This data files will be loaded at the installation (commented because file is not added in this example)
 	 'data': [
 	 	'security/groups.xml',
@@ -19,6 +19,7 @@ Description related to library.
 		'views/library_book.xml',
 		'views/library_book_categ.xml',
 		'views/library_book_rent.xml',
+		'views/templates.xml',
 		'data/library_stage.xml',
 		'report/book_rent_templates.xml',
 		'report/book_rent_report.xml',
